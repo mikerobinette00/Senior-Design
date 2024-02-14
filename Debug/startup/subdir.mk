91b23a504660a8f88560433145a8a1789cff2e96
mikerobinette00 <122401832+mikerobinette00@users.noreply.github.com>
2024-02-14 14:05:37 -0500

    semi functioning keypad
    0-9 on the keypad works.  Some code for driving spi display is in there as well.  Display not working correctly yet

--- Debug/startup/subdir.mk
+++ Debug/startup/subdir.mk
@@ -15,7 +15,7 @@ startup/%.o: ../startup/%.s
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Assembler'
 	@echo $(PWD)
-	arm-none-eabi-as -mcpu=cortex-m0 -mthumb -mfloat-abi=soft -I"U:/personal/SystemWorkbench2.9/Senior_Design/StdPeriph_Driver/inc" -I"U:/personal/SystemWorkbench2.9/Senior_Design/inc" -I"U:/personal/SystemWorkbench2.9/Senior_Design/CMSIS/device" -I"U:/personal/SystemWorkbench2.9/Senior_Design/CMSIS/core" -g -o "$@" "$<"
+	arm-none-eabi-as -mcpu=cortex-m0 -mthumb -mfloat-abi=soft -I"//NAS01.ITAP.PURDUE.EDU/puhome/My Documents/GitHub/Senior-Design/StdPeriph_Driver/inc" -I"//NAS01.ITAP.PURDUE.EDU/puhome/My Documents/GitHub/Senior-Design/inc" -I"//NAS01.ITAP.PURDUE.EDU/puhome/My Documents/GitHub/Senior-Design/CMSIS/device" -I"//NAS01.ITAP.PURDUE.EDU/puhome/My Documents/GitHub/Senior-Design/CMSIS/core" -g -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
